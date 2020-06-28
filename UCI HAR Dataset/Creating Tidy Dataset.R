ca6a1a87bb6bcf4a73964a2eea06b0e3f9212d8b
devpra1999 <srivastava.dev98@gmail.com>
2020-06-29 03:55:49 +0530

    Created a tidy dataset with summarised variables

---
+++ UCI HAR Dataset/Creating Tidy Dataset.R
@@ -0,0 +1,17 @@
+dataset <- read.delim("./combined/dataset.txt", sep = " ", stringsAsFactors = TRUE)
+
+#dataset$Activity <- as.factor(dataset$Activity)
+
+label <- levels(dataset$Activity)
+
+dataset$Activity <- as.numeric(dataset$Activity)
+
+split_factor <- list(dataset$Subject_ID, dataset$Activity)
+s <- split(dataset,split_factor)
+
+final_dataset <- as.data.frame( t(sapply(s, colMeans)) )
+
+
+final_dataset$Activity <- label[final_dataset$Activity]
+
+write.table(final_dataset, file = "./combined/final dataset.txt")
